7513a5c632d6c2acaae14ed77cbdfa90fb6248a2
adizele <adina.dizele@gmail.com>
2024-09-29 04:56:06 +0200

    Update db.js #1

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const util = require('util');
 // Redis
 const redisHost = process.env.REDIS_HOST || 'localhost';  // Utiliser 'localhost' comme valeur par défaut si non définie
 const redisPort = process.env.REDIS_PORT || 6379;         // Utiliser 6379 par défaut
-const redisPassword = REDIS_PASSWORD || '';
+const redisPassword = process.env.REDIS_PASSWORD || '';
 
 exports.CheckRedis = function () {
     //const checkRedis = redis.createClient();
